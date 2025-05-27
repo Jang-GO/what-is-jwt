3a65f154990bb4a83c1d967df9bc0ec471dc5211
Jang-GO <jang8195@naver.com>
2025-05-27 20:15:52 +0900

    initial commit

---
+++ src/main/java/org/janggo/whatisjwt/WhatisjwtApplication.java
@@ -0,0 +1,13 @@
+package org.janggo.whatisjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatisjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatisjwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/janggo/whatisjwt/WhatisjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.janggo.whatisjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatisjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
